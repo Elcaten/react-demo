dfe909b24aa04945efd38d4790b0cdaf0fa8a872
Andrew Lineyshikov <andrey.lineyshikov@arcadia.spb.ru>
2020-05-09 13:36:02 +0300

    Add weather icon

--- src/weather/weather.tsx
+++ src/weather/weather.tsx
@@ -11,23 +11,23 @@ export const Weather: React.FC<WeatherProps> = ({ position }) => {
     return (
         <main>
             <h1>Weather</h1>
-
             <section>
                 <h4>Loading state:</h4>
                 <p>{state.pending ? 'true' : 'false'}</p>
             </section>
-
             <section>
                 <h4>Error:</h4>
                 <p>{state.error ? `Error: ${state.error.cod} - ${state.error.message}` : 'Nope'}</p>
             </section>
-
             <section>
                 <h4>Data:</h4>
                 {state.data && state.data.name && state.data.main ? (
                     <>
                         <p>Weather in {state.data.name}:</p>
                         <ul>
+                            <li>
+                                <i className={`wi wi-owm-${state.data?.weather[0].id}`} />
+                            </li>
                             <li>Currently: {state.data.main.temp} degrees</li>
                             <li>Max: {state.data.main.temp_max} degrees</li>
                             <li>Min: {state.data.main.temp_min} degrees</li>
@@ -37,7 +37,6 @@ export const Weather: React.FC<WeatherProps> = ({ position }) => {
                     <p>Nope</p>
                 )}
             </section>
-
             <button onClick={fetchWeather}>Fetch weather</button>
         </main>
     );
